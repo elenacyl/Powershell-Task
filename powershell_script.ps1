3c2ea738ffe2227f1bdfa235896848c12aa325a6
Elena Choo <chooylelena@gmail.com>
2020-01-28 12:04:56 +0800

    Commit file

---
+++ powershell_script.ps1
@@ -0,0 +1,72 @@
+# Interactive menu to select function to be carried out
+function Show-Menu
+{
+     param (
+           [string]$Title = 'Menu'
+     )
+     cls
+     Write-Host "================ $Title ================"
+
+     Write-Host "1: Press '1' to create a Local User."
+     Write-Host "2: Press '2' to rename a Local User."
+     Write-Host "3: Press '3' to remove a Local User."
+     Write-Host "Q: Press 'Q' to quit."
+}
+
+# Function 1 - Create new local user
+function New-User
+{
+  Try {
+    $Fullname = Read-Host "Enter User's name"
+    $Description = Read-Host "Enter account description"
+    $Username = Read-Host "Enter username"
+    $Password = Read-Host -AsSecureString "Enter password"
+    New-LocalUser $Username -Password $Password -FullName $Fullname -Description $Description
+  } Catch {
+     Write-Host "Unable to create local user. Please try again later."-ForegroundColor Red`n
+  }
+}
+
+# Function 2 - Rename a local user
+function Rename-User
+{
+  Try {
+    $ExistingUser = Read-Host "Enter User to rename"
+    $NewName = Read-Host "Enter new name"
+    Rename-LocalUser -Name $ExistingUser -NewName $NewName
+  } Catch {
+    Write-Host "Unable to rename local user. Please try again later." -ForegroundColor Red`n
+  }
+}
+
+# Function 3 - Remove a local user
+function Remove-User
+{
+  Try {
+    $Username = Read-Host "Enter User to be removed"
+    Remove-LocalUser -Name $Username
+  } Catch {
+    Write-Host "Unable to remove local user. Please try again later."-ForegroundColor Red`n
+  }
+}
+
+# Main app to run
+do
+{
+     Show-Menu
+     $input = Read-Host "Please make a selection"
+     switch ($input)
+     {
+           '1' {
+                New-User
+           } '2' {
+                Rename-User
+           } '3' {
+                Remove-User
+           } 'q' {
+                return
+           }
+     }
+     pause
+}
+until ($input -eq 'q')
